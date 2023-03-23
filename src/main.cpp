8d4492a0fa90254a80320cd6449b3ef9bb929328
Maduki-tech <maduki.business@gmail.com>
2023-03-23 20:49:47 +0100

    added triange that can rotate

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,78 @@
-#ifdef __APPLE_CC__
+// import the right one
+#ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
 
-// Clears the current window and draws a triangle.
-void display() {
+void changeSize(int w, int h) {
 
-  // Set every pixel in the frame buffer to the current clear color.
-  glClear(GL_COLOR_BUFFER_BIT);
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if (h == 0)
+		h = 1;
 
-  // Drawing is done by specifying a sequence of vertices.  The way these
-  // vertices are connected (or not connected) depends on the argument to
-  // glBegin.  GL_POLYGON constructs a filled polygon.
-  glBegin(GL_POLYGON);
-    glColor3f(1, 0, 0); glVertex3f(-0.6, -0.75, 0.5);
-    glColor3f(0, 1, 0); glVertex3f(0.6, -0.75, 0);
-    glColor3f(0, 0, 1); glVertex3f(0, 0.75, 0);
-  glEnd();
+	float ratio =  w * 1.0 / h;
 
-  // Flush drawing command buffer to make drawing happen as soon as possible.
-  glFlush();
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+
+	// Reset Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the correct perspective.
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
 }
 
-// Initializes GLUT, the display mode, and main window; registers callbacks;
-// enters the main event loop.
-int main(int argc, char** argv) {
+float angle = 0.0f;
 
+void renderScene(void) {
 
+	// Clear Color and Depth Buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	// Reset transformations
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(	0.0f, 0.0f, 10.0f,
+				0.0f, 0.0f,  0.0f,
+				0.0f, 1.0f,  0.0f);
 
-  /* // Use a single buffered window in RGB mode (as opposed to a double-buffered */
-  /* // window or color-index mode). */
-  /* glutInit(&argc, argv); */
-  /* glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); */
-  /*  */
-  /* // Position window at (80,80)-(480,380) and give it a title. */
-  /* glutInitWindowPosition(80, 80); */
-  /* glutInitWindowSize(400, 300); */
-  /* glutCreateWindow("A Simple Triangle"); */
-  /*  */
-  /* // Tell GLUT that whenever the main window needs to be repainted that it */
-  /* // should call the function display(). */
-  /* glutDisplayFunc(display); */
-  /*  */
-  /* // Tell GLUT to start reading and processing events.  This function */
-  /* // never returns; the program only exits when the user closes the main */
-  /* // window or kills the process. */
-  /* glutMainLoop(); */
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
+	glBegin(GL_TRIANGLES);
+		glVertex3f(-2.0f,-2.0f, 0.0f);
+		glVertex3f( 2.0f, 0.0f, 0.0);
+		glVertex3f( 0.0f, 2.0f, 0.0);
+	glEnd();
+
+	angle+=0.1f;
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("Lighthouse3D- GLUT Tutorial");
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	return 1;
 }
